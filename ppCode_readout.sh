50a5156546355fba6c49afdca403c612f4b827dd
John Beale <jbeale1@users.noreply.github.com>
2018-07-20 08:12:02 -0700

    Create ppCode_readout.sh

---
+++ ppCode_readout.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Get Temp (deg. F) and Humidity (%) from pp-Code Watchman wifi sensor
+# made from NodeMcu V3 wifi board + AM2302 (wired DHT22) sensor
+# https://www.amazon.com/pp-Code-Temperature-sensor-monitor-party-detect/dp/B07D23G27R/
+
+IP=192.168.1.116     # IP address of sensor device on LAN
+IDNUM=abcd12341234   # unique ID of sensor (label on bottom)
+LOG="tlog.txt"       # output file with data in CSV format
+
+OUT="http://"$IP"/"$IDNUM"&Stats"  # http string to retrieve temp & humid. readings
+
+# output format:   YYYY-MM-DD HH:MM:SS , TEMP(F) , RH(%)
+
+while [ true ]; do
+  
+  IN=$( wget $OUT -qO- )
+  IFS=':' read -ra AR <<< "$IN"
+  td=$(date +%F)" "$(date +%T)" ,"
+
+  echo -n $td >> $LOG
+  s=${AR[1]} ;  echo -n " ${s%%F*} , " >> $LOG
+  s=${AR[2]} ;  echo "${s%%\%*}" >> $LOG
+  
+  sleep $((60 - $(date +%S) ))  # wait until top of next minute
+
+done
