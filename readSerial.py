66e9f38508e41e7e21a2d4f8d5692488de1f9299
John Beale <jbeale1@users.noreply.github.com>
2018-09-08 19:10:20 -0700

    remove unwanted null byte at input

--- readSerial.py
+++ readSerial.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
-# Read CSV text from serial port (USB adaptor) eg. Arduino 
 # tested with Python 3.6.5 (default, Apr  1 2018, 05:46:30) 
 import serial
 
-logfile='temp-log.csv'
+logfile='/home/myshake/serlog/temp-log.csv'
 
 f = open(logfile, 'w')
 fctr = 8
@@ -13,11 +12,12 @@ with serial.Serial('/dev/ttyUSB0', 9600, timeout=10) as ser:
   while True:
     line = ser.readline()
     # print(line)
-    s = line.decode("utf-8").strip()  # bytes to string, without start/end whitespace
+    s0 = line.decode("utf-8").strip()  # bytes to string, without start/end whitespace
+    s = s0.strip('\0')  # serial port open sometimes gives a null byte
+    nc = len(s)  # how many useful characters in the input string?
     s = s + "\n"
-    nc = len(s)  # how many characters in the string?
     if ( nc > 0 ):
-      print(s,end='')
+      # print(s,end='')
       f.write(s)
       fctr += 1
       if (fctr > 10):
