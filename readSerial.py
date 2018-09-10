93169af36b837cc70ea6857062d0dd40cd3d6fb7
John Beale <jbeale1@users.noreply.github.com>
2018-09-09 21:47:37 -0700

    add time & date to each sample

--- readSerial.py
+++ readSerial.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 
 # tested with Python 3.6.5 (default, Apr  1 2018, 05:46:30) 
-import serial
+import serial, datetime
 
 logfile='/home/myshake/serlog/temp-log.csv'
+FLIM = 10  # flush buffer after this many lines
 
 f = open(logfile, 'w')
-fctr = 8
+
+fctr = 8  # saved-line counter (flush buffer after FLIM lines)
+pctr = 5  # sample decimation counter 
+firstline = 1  # have not yet finished the first line
 
 with serial.Serial('/dev/ttyUSB0', 9600, timeout=10) as ser:
   while True:
@@ -18,9 +22,19 @@ with serial.Serial('/dev/ttyUSB0', 9600, timeout=10) as ser:
     s = s + "\n"
     if ( nc > 0 ):
       # print(s,end='')
-      f.write(s)
-      fctr += 1
-      if (fctr > 10):
-        f.flush()
-        fctr = 0
-    # print(s,end='')
+      pctr += 1
+      if (pctr > 4):  # only print every 5th line to save space
+        pctr = 0
+
+        if (firstline == 1):
+          oline = "date_time, " + s
+          firstline = 0
+        else:
+          oline = str(datetime.datetime.now()) + ", " + s
+
+        f.write(oline)
+        fctr += 1
+        if (fctr > FLIM):
+          f.flush()
+          fctr = 0
+      # print(s,end='')
