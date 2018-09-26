a563c46fd58a856e0a4cc268020538bfa0033705
John Beale <jbeale1@users.noreply.github.com>
2018-09-26 13:08:12 -0700

    append different version, other parameters

--- audio-24hr-record.sh
+++ audio-24hr-record.sh
@@ -10,3 +10,25 @@
 ffmpeg -nostdin -loglevel quiet -f alsa -ac 1 -ar 8000 -i plughw:1 -map 0:0 -acodec libmp3lame \
   -b:a 64k -f segment -strftime 1 -segment_time 900 -segment_atclocktime 1 \
    ChA_%Y-%m-%d_%H-%M-%S.mp3
+
+# ============== alternate version ===========================
+
+#!/bin/bash
+
+# location to store audio files
+#todir="/run/shm/audio/"
+todir="./"
+
+# number of seconds each .mp3 file should last
+duration="900"
+
+# which audio device card number to work with
+cnum="1"
+
+# set mic input level to ~ 0 dB (which is strangely 15 out of 100)
+amixer -c "$cnum" cset numid=3 15
+
+# Record 5-minute-long MP3 files from audio device
+ffmpeg -f alsa -ac 2 -ar 48000 -i plughw:"$cnum" -map 0:0 -acodec libmp3lame \
+  -b:a 256k -f segment -strftime 1 -segment_time "$duration" -segment_atclocktime 1 \
+   "$todir"ChA_%Y-%m-%d_%H-%M-%S.mp3
