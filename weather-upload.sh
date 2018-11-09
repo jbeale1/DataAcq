4a17c4e4e01dc267c38e6c8f726e9b55ac7b671d
John Beale <jbeale1@users.noreply.github.com>
2018-11-09 14:04:04 -0800

    upload PiKrellCam weather image to WUnderground

---
+++ weather-upload.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# get latest still image, crop, rescale, and
+# upload JPEG to Wunderground webcam page
+# Can be run every 5 min from crontab with:
+#  */5 * * * * /dev/shm/weather-upload.sh
+
+# Original PKC image size with this cam is 3280x2464 
+XS="3150"  # cropped X size before rescale
+YS="2464"  # cropped Y size before rescale
+
+DIR="/home/pi/pikrellcam/media/stills"   # get image from here
+FIFO="/home/pi/pikrellcam/www/FIFO"      # control camera here
+
+HOST="webcam.wunderground.com"           # upload to WU here
+USER="my_username"                       # my assigned WU username
+PASSWD="my_password"                     # my assigned WU password
+
+INFILE="latest.jpg"                      # local temp filename
+OUTFILE="image.jpg"                      # required filename for WU
+
+# send command into PiKrellCam FIFO to capture a still image right now:
+echo "still" > $FIFO
+
+# wait for PKC command to be executed and file to be written to disk
+sleep 4
+
+# find most recent file in PKC stills directory
+unset -v latest
+for file in "$DIR"/*.jpg; do
+  [[ $file -nt $latest ]] && latest=$file
+done
+
+# copy latest image to working directory
+cp $file $INFILE
+
+# crop off right-hand edge, rescale to 25% size, and force size to be within 150 kB
+convert $INFILE -crop "$XS"x"$YS"+0+0 -resize 25% -define jpeg:extent=150000 $OUTFILE
+
+# send image to remote host via FTP
+ftp -n -v $HOST << EOT
+user $USER $PASSWD
+prompt
+binary
+put $OUTFILE 
+bye
+EOT
+
+exit 0
