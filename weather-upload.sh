45de498b9f311f442216e42c12957a93e6146734
John Beale <jbeale1@users.noreply.github.com>
2018-11-10 09:21:11 -0800

    re-fix pathnames

--- weather-upload.sh
+++ weather-upload.sh
@@ -12,6 +12,7 @@
 # su pi -c '(sleep 5; /usr/local/bin/pikrellcam) &'
 
 
+
 # Original PKC image size with this cam is 3280x2464 
 XS="3150"  # cropped X size before rescale
 YS="2464"  # cropped Y size before rescale
@@ -24,8 +25,8 @@ HOST="webcam.wunderground.com"           # upload to WU here
 USER="my_username"                       # my assigned WU username
 PASSWD="my_password"                     # my assigned WU password
 
-INFILE=$WDIR"/latest.jpg"                      # local temp filename
-OUTFILE=$WDIR"/image.jpg"                      # required filename for WU
+INFILE="latest.jpg"                      # local temp filename
+OUTFILE="image.jpg"                      # required filename for WU
 
 # send command into PiKrellCam FIFO to capture a still image right now:
 echo "still" > $FIFO
@@ -43,7 +44,7 @@ done
 cp $file $INFILE
 
 # crop off right-hand edge, rescale to 25% size, and force size to be within 150 kB
-convert $INFILE -crop "$XS"x"$YS"+0+0 -resize 25% -define jpeg:extent=150000 $OUTFILE
+convert $WDIR/$INFILE -crop "$XS"x"$YS"+0+0 -resize 25% -define jpeg:extent=150000 $WDIR/$OUTFILE
 
 # send image to remote host via FTP
 ftp -n -v $HOST << EOT
