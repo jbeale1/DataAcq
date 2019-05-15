54de2ecd76174d0f7ede199447366ae1efe25c7e
John Beale <jbeale1@users.noreply.github.com>
2019-05-14 23:03:04 -0700

    improve formatting

--- MCP3424_read.py
+++ MCP3424_read.py
@@ -13,9 +13,8 @@ adc_address = 0x69  # MCP3424 I2C address selection
 # b7: RDY 1-shot mode:, 1=start conversion
 # b6,b5: C1-C0: Channel 1-4: 00, 01, 10, 11
 # b4: Convert Mode 1 = Continuous, 0 = One-Shot single convert
-# b3,b2: Sample Rate 00=240 SPS (12 bits), 01 = 60 SPS (14 bits)
-#  10 = 15 SPS (16 bits),  11 = 3.75 SPS (18 bits)
-# b1,b00  G1-G0: PGA Gain 00 = x1  01 = x2  10 = x4   11 = x8
+# b3,b2: Rate+Res 00=240Hz/12bit 01=60/14bit 10=15/16bit 11=3.75/18 bit
+# b1,b0:  G1-G0: PGA Gain 00 = x1  01 = x2  10 = x4   11 = x8
 
 #  1-shot, Ch1, 18 bits/3.75 sps, PGA Gain=x1   1000 1100 = 0x8c
 #  1-shot, Ch2, 18 bits/3.75 sps, PGA Gain=x1   1010 1100 = 0xac
@@ -46,15 +45,12 @@ def getadreading(address,adcConfig):
 
 # ====================
 
-# start ADC conversion, but current value is junk because previous settings unknown
 t = datetime.datetime.utcnow()
-
 print("# MCP3424 4-Ch ADC log v0.1")
 print("# Start: " + str(t) + " UTC")
 
+# restart ADC conversion + read prev result which is junk because previous settings unknown
 junk = getadreading(adc_address, channels[0]) # I2C address, config register with channel #
-
-# oldsec = t.second + t.microsecond/1000000.0
 
 while ( True ):
   # sleep(1/3.75) # MCP3424: 3.75 samples per second at 18 bit resolution
@@ -69,7 +65,6 @@ while ( True ):
     next = (c + 1) % cnum  # channel number to read next: 2,3,0
     raw[c] = getadreading(adc_address, channels[next]) # I2C address, config register with channel #
 
-
   for i in range(0,cnum):
     print( "{:+.{}f}".format( raw[i], 6 ),end=' , ' )
-  print( "{0:.3f}".format(sectime) )
+  print( "{0:.3f}".format(sectime) )  # second of the UTC minute [0...60)
