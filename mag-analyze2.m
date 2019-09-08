cc1e9e45c9057c82a4bd1749354b0b57b86a1499
John Beale <jbeale1@users.noreply.github.com>
2019-09-07 20:51:02 -0700

    Update mag-analyze2.m

--- mag-analyze2.m
+++ mag-analyze2.m
@@ -10,19 +10,25 @@ cStart = clock();
 dnStart = datenum(cStart); # day number from Year 0000
 dstr = datestr(dnStart);
 
-fstart = [ 2019 09 06 10 45 45 ]; # file start time
+# fstart = [ 2019 09 07 14 13 (23.335) ]; # 190907-MagTest1 start
+fstart = [ 2019 09 07 17 49 35.359631 ]; # 190907-MagTest2 start
+fname = "/home/john/magfield/190907-MagTest2.csv";  # input data
+
 fDnum = datenum(fstart);  # file-start Day Number
 fDstr = datestr(fDnum);
 printf("Runtime: %s  File start: %s\n",dstr,fDstr);
 
-
 # ------------------------------------------------------------------
 sample_rate = 7.457716;  # LTC2400 ADC sample rate, Hz
+#sample_rate = 7.47372; # rate if an extra datestamp line every minute
 upsample_ratio = 10;   # upsample raw data to resolve xcorr peak
 xwindow = 40;          # window size: samples around each event
-xstart=7000;           # ignore nose/data before this line
-rThresh = 0.10;        # threshold on xcorr peak value for valid event
-fname = "/home/john/magfield/190906-MagSenseRd_1137am.csv";  # input data
+# xstart=7000;           # ignore nose/data before this line
+xstart=3;           # ignore nose/data before this line
+rThresh = 0.002;        # threshold on xcorr peak value for valid event
+pkThresh = 0.005;       # initial raw peak threshold
+sPolarity = -1;         # fudge factor of 1 or -1 if sensor order switched
+
 off1=30;          # low side window offset from filtered peak center
 off2=10;          # high side window offset from filtered peak center
 
@@ -39,13 +45,10 @@ sum=raw(:,1) + raw(:,2);  # blindly add ch1 and ch2 together
 [b, a] = butter(2, 0.05); # lowpass filter
 filt = filter(b,a,abs(sum)); # smooth out combined data
 
+# simple peak-find based on ch.1 + ch.2
 warning ("off");
-#[pks idx] = findpeaks(raw(:,1),
-#  "MinPeakHeight",0.005,"MinPeakDistance",20,"MinPeakWidth",8,"DoubleSided");
 [pks2 idx2] = findpeaks(max(0,filt),
-    "MinPeakHeight",0.010,"MinPeakDistance",10,"MinPeakWidth",8);
-#[pks3 idx3] = findpeaks(sum,
-#    "MinPeakHeight",0.005,"MinPeakDistance",40,"MinPeakWidth",8,"DoubleSided");
+    "MinPeakHeight",pkThresh,"MinPeakDistance",10,"MinPeakWidth",8);
 warning ("on");
 t=1:rows(raw);
 
@@ -59,10 +62,16 @@ t=1:rows(raw);
 #subplot(2,1,1)
 #plot(t,raw(:,1),t(idx),raw(idx,1),'or'); axis tight;
 #subplot(2,1,2)
-plot(t,filt,t(idx2),filt(idx2),'or'); axis tight;
+lfilt=log(abs(filt)+0.001);
+plot(t,lfilt,t(idx2),lfilt(idx2),'or'); axis tight;
 #rows(pks)   # how many peaks found
 #rows(pks2)
 #break;
+
+# ======================
+#s = tf('s');
+#g = 1/(2*s^2+3*s+4);
+#h = impulse(g);
 
 eventIdx = 0;  # keep track of each event
 xs = xwindow; # x step size (= or smaller than window size)
@@ -74,7 +83,6 @@ for i = (1 : rows(idx2) );
   [rpk, ridx] = max(R);  # if rpk is too low => no real event
 
   if (rpk > rThresh)
-    eventIdx += 1;
     [pk idx] = max(abs(s1));  # ch1,2 peak and index
     PkIdx1 = idx(1) + (c-off1); # off by 1?
     PkIdx2 = idx(2) + (c-off1); # off by 1?
@@ -83,15 +91,23 @@ for i = (1 : rows(idx2) );
     lead = ceil(e1/2) - ridx;  # ans = 89 (samples by which ch1 leads ch2)
     dt = lead / (sample_rate * upsample_ratio); # seconds by which ch1 leads ch2)
     dist = 7.5 + 4.6 - 0.01; # 12.09 meters
-    speed_ms = dist / dt;  # meters per second
-    speed_mph = speed_ms * 2.23694;
-    start_min = PkIdx2 / (sample_rate * 60);
-    eventDnum = fDnum + start_min / (60*24);  # day number of this event
-    eventDstr = datestr(eventDnum); # Y M D H:M:S format
-    printf("%02d : %s  Rpk = %05.2f : %+05.2f mph\n",
-       eventIdx,eventDstr,rpk,speed_mph);
-    # printf("Xcor: %3.1f  Speed: %5.2f mph\n",rpk,speed_mph);
-    # break;
+    if (abs(lead)>1)
+      speed_ms = dist / dt;  # meters per second
+      speed_mph = sPolarity * speed_ms * 2.23694;
+    else
+      speed_mph = 1E6;
+    endif
+    if (abs(speed_mph) < 65) # don't be ridiculous
+      eventIdx += 1;
+      start_min = PkIdx1 / (sample_rate * 60);
+      # start_min = PkIdx2 / (sample_rate * 60);
+      eventDnum = fDnum + start_min / (60*24);  # day number of this event
+      eventDstr = datestr(eventDnum); # Y M D H:M:S format
+      printf("%02d, %s, %05.2f, %+05.2f\n",
+         eventIdx,eventDstr,rpk,speed_mph);
+      # printf("Xcor: %3.1f , %5.2f mph\n",rpk,speed_mph);
+      # break;
+    endif
 
   endif
 
@@ -99,49 +115,49 @@ endfor
 # -----------------------------------------------
 
 #{
-sensor1 time at drvway	R-val	 MPH	     desc
-10:45:59 AM	10:46:00 AM	0.17	-27.26   L slv 4D
-10:46:05 AM	10:46:07 AM	10.89	+27.63   R gry crsovr
-10:47:04 AM	10:47:06 AM	4.5	  +25.53   R slvr crsovr
-10:48:16 AM	10:48:18 AM	2.93 	+30.56   R grn jeep
-          	10:48:38 AM			           L wht truck/van
-10:49:09 AM	10:49:11 AM	3.87	+23.45	 R blu SUV
-10:49:22 AM	10:49:24 AM	19.98	+29.66	 R slvr 4D
-10:49:32 AM	10:49:34 AM	5.83	+30.10   R wht 4D
-10:50:06 AM	10:50:08 AM	0.68	+17.39	 R red pickup
-10:50:18 AM	10:50:20 AM	5.99	+26.19	 R grey SUV
-10:51:23 AM	10:51:25 AM	3.6	  +24.3	   R gry 4D
-10:52:26 AM	10:52:27 AM	0.36	-31.03	 L slvr 4D
-10:53:19 AM	10:53:20 AM	0.11	-27.63	 L gry 4D
-10:53:23 AM	10:53:24 AM	0.65	-28.81	 L red SUV
-10:53:45 AM	10:43:46 AM	0.37	-34.77	 L slvr 4D
-10:56:11 AM	10:56:13 AM	5.47	+26.54	 R wht pickup
-10:56:40 AM	10:56:43 AM	10.8	+21.23	 R blk SUV
-10:57:50 AM	10:57:52 AM	9.74	+16.95	 R grey 4D
-10:58:59 AM	10:59:01 AM	2.8	  +26.89	 R turq jeep
-11:00:56 AM	11:00:57 AM	0.29	-33.06	 L slvr SUV
-           	11:03:34 AM			           L FedEx Trk
-	          11:03:41 AM			           L blk crsovr
-11:05:04 AM	11:05:06 AM	0.17	-26.19	 L wht 4D
-11:05:53 AM	11:05:55 AM	4.87	+30.56	 R wht prius
-11:08:14 AM	11:08:15 AM	0.3	  -28.81	 L slvr 4D
-11:09:52 AM	11:09:53 AM	0.2	  -28.81	 L dkrd 4D
-11:12:08 AM	11:12:10 AM	0.13	-34.77	 L red 4D
-11:14:03 AM	11:14:05 AM	0.68	+24.01	 R white 4D
-11:14:53 AM	11:14:55 AM	0.76	+29.23	 R drk crsover
-11:15:03 AM	11:15:05 AM	4.98	+26.19	 R silver crsovr
-	          11:16:21 AM         			 L red crossover
-11:16:34 AM	11:16:36 AM	5.94	+22.16	 R silver 4D
-11:17:55 AM	11:17:57 AM	4.73	+24.90   R silver crossover
-11:18:04 AM	11:18:05 AM	0.25	-31.03	 L grey crossover
-11:18:27 AM	11:18:29 AM	0.32	+26.89	 R grey prius
-11:21:26 AM	11:21:28 AM	3.22	+26.89	 R black 4D
-11:22:01 AM	11:22:02 AM	0.87	-31.51	 L silver 4D
-11:24:02 AM	11:24:03 AM	0.16	-31.51	 L silver compact
-          	11:26:01 AM         			 L dark 4D
-11:27:09 AM	11:27:17 AM	6.38	+25.21	 R silver 4D
-11:31:14 AM	11:31:15 AM	16.88	+22.66	 R silver 4D
-11:32:08 AM	11:32:10 AM	1.27	+30.56	 R black pickup
-11:35:01 AM	11:35:00 AM	5.02	-87.69	 error:L truck + 2 R cars
-11:35:59 AM	11:36:02 AM	1.00  +28.81	 R wht 4D
+sensor1 time at drvway  R-val   MPH       desc
+10:45:59 AM  10:46:00 AM  0.17  -27.26   L slv 4D
+10:46:05 AM  10:46:07 AM  10.89 +27.63   R gry crsovr
+10:47:04 AM  10:47:06 AM  4.5   +25.53   R slvr crsovr
+10:48:16 AM  10:48:18 AM  2.93  +30.56   R grn jeep
+10:48:37 AM  10:48:38 AM  0.01  -15.88   L wht truck/van
+10:49:09 AM  10:49:11 AM  3.87  +23.45   R blu SUV
+10:49:22 AM  10:49:24 AM  19.98 +29.66   R slvr 4D
+10:49:32 AM  10:49:34 AM  5.83  +30.10   R wht 4D
+10:50:06 AM  10:50:08 AM  0.68  +17.39   R red pickup
+10:50:18 AM  10:50:20 AM  5.99  +26.19   R grey SUV
+10:51:23 AM  10:51:25 AM  3.6   +24.3    R gry 4D
+10:52:26 AM  10:52:27 AM  0.36  -31.03   L slvr 4D
+10:53:19 AM  10:53:20 AM  0.11  -27.63   L gry 4D
+10:53:23 AM  10:53:24 AM  0.65  -28.81   L red SUV
+10:53:45 AM  10:43:46 AM  0.37  -34.77   L slvr 4D
+10:56:11 AM  10:56:13 AM  5.47  +26.54   R wht pickup
+10:56:40 AM  10:56:43 AM  10.8  +21.23   R blk SUV
+10:57:50 AM  10:57:52 AM  9.74  +16.95   R grey 4D
+10:58:59 AM  10:59:01 AM  2.8   +26.89   R turq jeep
+11:00:56 AM  11:00:57 AM  0.29  -33.06   L slvr SUV
+11:03:33 AM  11:03:34 AM  0.03  -13.27   L FedEx Trk
+11:03:39 AM  11:03:41 AM  0.07  -21.01   L blk crsovr
+11:05:04 AM  11:05:06 AM  0.17  -26.19   L wht 4D
+11:05:53 AM  11:05:55 AM  4.87  +30.56   R wht prius
+11:08:14 AM  11:08:15 AM  0.3   -28.81   L slvr 4D
+11:09:52 AM  11:09:53 AM  0.2   -28.81   L dkrd 4D
+11:12:08 AM  11:12:10 AM  0.13  -34.77   L red 4D
+11:14:03 AM  11:14:05 AM  0.68  +24.01   R white 4D
+11:14:53 AM  11:14:55 AM  0.76  +29.23   R drk crsover
+11:15:03 AM  11:15:05 AM  4.98  +26.19   R silver crsovr
+11:16:19 AM  11:16:21 AM  0.04  -33.62   L red crossover
+11:16:34 AM  11:16:36 AM  5.94  +22.16   R silver 4D
+11:17:55 AM  11:17:57 AM  4.73  +24.90   R silver crossover
+11:18:04 AM  11:18:05 AM  0.25  -31.03   L grey crossover
+11:18:27 AM  11:18:29 AM  0.32  +26.89   R grey prius
+11:21:26 AM  11:21:28 AM  3.22  +26.89   R black 4D
+11:22:01 AM  11:22:02 AM  0.87  -31.51   L silver 4D
+11:24:02 AM  11:24:03 AM  0.16  -31.51   L silver compact
+11:26:00 AM  11:26:01 AM  0.03  -21.69   L dark 4D
+11:27:09 AM  11:27:17 AM  6.38  +25.21   R silver 4D
+11:31:14 AM  11:31:15 AM  16.88 +22.66   R silver 4D
+11:32:08 AM  11:32:10 AM  1.27  +30.56   R black pickup
+11:35:01 AM  11:35:00 AM  5.02  -87.69   error:L truck + 2 R cars
+11:35:59 AM  11:36:02 AM  1.00  +28.81   R wht 4D
 #}
