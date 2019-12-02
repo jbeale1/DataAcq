42c8465dacd974233dfdb9fadd43b584ad56317f
John Beale <jbeale1@users.noreply.github.com>
2019-12-02 11:55:18 -0800

    increase accuracy, 2 kHz output

--- PPS-Pi-GPIO.c
+++ PPS-Pi-GPIO.c
@@ -1,6 +1,6 @@
-// PIGPIO example code
-// gcc -Wall -pthread -o ftest1 ftest1.c -lpigpio -lrt
-// send out pulse train on GPIO pin at top of each second
+// based on PIGPIO example code
+// compile with: gcc -Wall -pthread -o ptest1 ptest1.c -lpigpio -lrt
+// Purpose: send out pulse train on GPIO pin starting at top of each second
 // J.Beale 01-DEC-2019
 
 #include <stdio.h>
@@ -9,30 +9,44 @@
 #include <unistd.h>
 #include <sys/time.h>  // time of day tv_sec, tv_usec
 
-#include <pigpio.h>
+#include <pigpio.h> // pigpio.zip from http://abyz.me.uk/rpi/pigpio/download.html
 
 
 int pnum = 2;  // GPIO 02
-uint32_t dTarget = 200;  // microseconds to delay
+uint32_t dHi = 250;  // duration of output high pulse, microseconds
+uint32_t dLo = 250;  // duration of output low pulse, microseconds
+int pulses = 5;  // count of pulses in pulse train output
+
+// compensated delay: shorten this wait by amount the previous wait ran over
+int compDelay(int T, int previous) {
+int startTick, realDelay;
+
+    int newDelay = 2*T - previous;
+    if (newDelay > 0) {
+      startTick = gpioTick();
+      gpioDelay(newDelay/3);  // longer delays turn out to be less accurate
+      gpioDelay(newDelay/3);
+      gpioDelay(newDelay/3);
+      realDelay = gpioTick() - startTick;
+    } else realDelay = 0;  // actually slightly more than 0 but...
+    return(realDelay);
+}
+
 
 int main(int argc, char *argv[]) {
 
-uint32_t startTick, endTick; // sys tick, the number of microseconds since system boot.
-int diffTick;
 struct timeval currentTime;  // for current time of day
 uint32_t realDelay;  // actual microseconds that delay routine took
 long int usec[4], sec[4];  // current seconds and microseconds
 
   if (gpioInitialise() < 0)
   {
-   // pigpio initialisation failed.
    printf("PIGPIO init failed.\n");
    return -1;
   }
 
   gpioSetMode(pnum, PI_OUTPUT); // Set GPIO pin as output.
   gpioSetPullUpDown(pnum, PI_PUD_OFF);  // Clear any pull-ups/downs.
-
 
   do {
 
@@ -43,37 +57,21 @@ long int usec[4], sec[4];  // current seconds and microseconds
   long int dGoal = (1e6 - usec[0]) - 80;  // how long until top of next second
   if (dGoal > 0) realDelay = gpioDelay(dGoal);
   else realDelay = 0;
-
-  long int rdiff = realDelay - dGoal;
-  gettimeofday(&currentTime, NULL);
-  usec[1] = currentTime.tv_usec;
-  sec[1] = currentTime.tv_sec;
   
-  startTick = gpioTick();
+  realDelay = dLo;    // initialize to the nominal value
 
-  for (int i=0;i<5;i++) {
-    gpioWrite(pnum, 1); // Set GPIO[pnum] high.
-    realDelay = gpioDelay(dTarget);
-    gpioWrite(pnum, 0); // Set GPIO[pnum] high.
-    int32_t delta = realDelay - dTarget;
-    int32_t newDelay = dTarget - delta;
-    if (newDelay > 0) {
-      realDelay = gpioDelay(newDelay);
-    }  
+  for (int i=0;i<pulses;i++) {
+    gpioWrite(pnum, 1); // Set GPIO[pnum] high 
+    realDelay = compDelay(dHi, realDelay);
+    gpioWrite(pnum, 0); // Set GPIO[pnum] low
+    realDelay = compDelay(dLo, realDelay);
   }
-
-  endTick = gpioTick();
-  diffTick = endTick - startTick;
 
   gettimeofday(&currentTime, NULL);
   usec[2] = currentTime.tv_usec;
   sec[2] = currentTime.tv_sec;
 
-  printf("Initial time: sec: %ld  usec: %ld\n",sec[0],usec[0]);
-  printf("actual excess delay: %ld\n",rdiff);
-  printf("Start time: sec: %ld  usec: %ld\n",sec[1],usec[1]);
-  printf("End time: sec: %ld  usec: %ld\n",sec[2],usec[2]);
-  printf("Elapsed systick time %d microseconds\n", diffTick);
+  printf(" sec: %ld  usec: %ld\n",sec[2],usec[2]);
 
   } while (1);
 
