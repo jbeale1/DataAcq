60735575f7e5c5a906ecf302d34fba2e6bb049f0
John Beale <jbeale1@users.noreply.github.com>
2019-12-02 20:31:53 -0800

    fix rare timing bug

--- PPS-Pi-GPIO.c
+++ PPS-Pi-GPIO.c
@@ -15,18 +15,20 @@
 
 
 int pnum = 2;  // GPIO 02
-uint32_t dSP = 250;  // duration of high pulse, microseconds (short tick)
-uint32_t dLP = 500;  // duration of high pulse, us (longer tone)
+uint32_t dSP = 247;  // duration of high pulse, microseconds (short tick)
+uint32_t dLP = 424;  // duration of high pulse, us (longer tone) (~ 1 kHz)
 int tickPulses = 5;  // count of pulses in pulse train output
-int minutePulses = 300;  // count of pulses for minute mark
-int delay30s = 100000;    // usec delay between double-tick at 30s mark
+int minutePulses = 100;  // count of pulses for minute mark
+int delay30s = 75000;    // usec delay between double-tick at 30s mark
+int fudge = 90;           // extra fudge factor to adjust delay
 
 // compensated delay: shorten this wait by amount the previous wait ran over
 // p : 1 for higher precision delay
-int compDelay(int T, int previous, int p) {
+int compDelay(int T, int p) {
+
 int startTick, realDelay;
 
-    int newDelay = 2*T - previous;
+    int newDelay = T;
     if (newDelay > 0) {
       startTick = gpioTick();
       if (p==1) {
@@ -42,16 +44,13 @@ int startTick, realDelay;
 }
 
 // generate a pulse train containing P pulses, prec=1 for higher-precision delay
-void sigGen(int P, int Dur, int prec) {
+void sigGen(int Pcount, int Dur, int prec) {
 
-uint32_t realDelay;
-
-  realDelay = Dur;
-  for (int i=0;i<P;i++) {
+  for (int i=0; i<Pcount; i++) {
     gpioWrite(pnum, 1); // Set GPIO[pnum] high 
-    realDelay = compDelay(Dur, realDelay, prec);
+    compDelay(Dur, prec);
     gpioWrite(pnum, 0); // Set GPIO[pnum] low
-    realDelay = compDelay(Dur, realDelay, prec);
+    compDelay(Dur, prec);
   }
 }
 
@@ -79,21 +78,25 @@ char time_string[40]; // time/date string
   gettimeofday(&currentTime, NULL);
   usec[0] = currentTime.tv_usec;
   
-  // Note: 80us is a fudge factor which improves accuracy on my Pi3B unit
-  long int dGoal = (1e6 - usec[0]) - 80;  // this long until top of next second
+  // Note: 70 usec fudge factor improves accuracy on my Pi3B unit
+  long int dGoal = (1e6 - usec[0]) - fudge;  // this long until top of next second
+  // printf("dGoal = %ld ",dGoal);
   if (dGoal > 0) gpioDelay(dGoal);
   
   // send out pulse train to mark start of a new second
   switch (seconds) {
     case 59:  // now reached top of the minute?
       sigGen(minutePulses, dLP, 0); // longer tone for minute mark
+      // gpioDelay(delay30s);  // pause between ticks
+      // sigGen(minutePulses, dLP, 0); // longer tone for minute mark
       break;
-    case 58:  // omit tick at 59 seconds
+    case 58:  // do not tick at 59 seconds
+      gpioDelay(delay30s);  // pause without ticks
       break;
     case 29:  // now reached the half-minute (30 sec)?
-      sigGen(tickPulses, dSP, 1); // double tick
+      sigGen(tickPulses, dSP, 1); // first tick
       gpioDelay(delay30s);  // pause between ticks
-      sigGen(tickPulses, dSP, 1); 
+      sigGen(tickPulses, dLP, 1); // second tick
       break;
     default:
       sigGen(tickPulses, dSP, 1); // normal second tick
@@ -106,7 +109,7 @@ char time_string[40]; // time/date string
   seconds = ptm->tm_sec;  // current time, seconds of the minute
   strftime (time_string, sizeof (time_string), "%Y-%m-%d %H:%M:%S", ptm);
   printf(" %s  usec: %ld\n", time_string, usec[2]);
-  gpioDelay(100000);  // make sure we're safely away from the start of the second
+  // gpioDelay(delay30s);  // make sure we're safely away from the start of the second
 
   } while (1);
 
