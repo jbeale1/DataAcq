1514b1fff3f84064ecd69b5e2a3e29ae8c9e64df
John Beale <jbeale1@users.noreply.github.com>
2020-04-22 23:47:27 -0700

    steps through 1 day of data

--- RShake_EventFind.py
+++ RShake_EventFind.py
@@ -4,19 +4,25 @@
 
 import numpy as np
 from scipy.signal import butter, lfilter, filtfilt
+from scipy.ndimage import uniform_filter1d
 import matplotlib.pyplot as plt
 
 from obspy import read
 import obspy.signal
 
-hpfreq=10.0  # remove frequencies below this (Hz)
+#hpfreq=10.0  # remove frequencies below this (Hz)
 
 ndir="/home/john/RShake"
 
-file="AM.R79D5.00.EHZ.D.2020.113"  # newer, PiZero @ 100 Hz
-#file="AM.RF7DC.00.SHZ.D.2020.113" # older, Pi3 @ 50 Hz
+#file="AM.RF7DC.00.SHZ.D.2020.112"
+filename="AM.R79D5.00.EHZ.D.2020.112"  # newer, PiZero @ 100 Hz
+#filename="AM.R79D5.00.EHZ.D.2020.113"  # newer, PiZero @ 100 Hz
+#filename="AM.RF7DC.00.SHZ.D.2020.113" # older, Pi3 @ 50 Hz
 
-fname=ndir+"/"+file
+cutoff = 0.08                # LP filter cutoff, in Hz units
+
+
+fname=ndir+"/"+filename
 # =========================================================
 
 def butter_lowpass(cutoff, fs, order=5):
@@ -35,6 +41,12 @@ def LP_zerophase_filter(data, cutoff, fs, order=3):
     y = filtfilt(b, a, data)
     return y
 
+def boxcar_filter(data, M):
+    y = uniform_filter1d(data, size=M, mode='reflect')
+    return y
+
+
+
 # ---------------------------------------------------
 
 st = read(fname)  # load example seismogram
@@ -47,24 +59,38 @@ dt = st[0].stats.starttime  # date/time of initial sample
 # Filter specs
 order = 3
 fs = st[0].stats.sampling_rate
-cutoff = 0.06  # in Hz units
+BoxSize = int(fs/cutoff)     # boxcar filter size
 
 b, a = butter_lowpass(cutoff, fs, order)  # generate filter coefficients
 
-st.filter('bandpass', freqmin=8, freqmax=12, corners=2, zerophase=True)
+st.filter('bandpass', freqmin=8, freqmax=20, corners=2, zerophase=True)
 
 data_env = obspy.signal.filter.envelope(st[0].data)
-envf = LP_zerophase_filter(data_env, cutoff, fs, order)  # LP filtered version
+#envf = LP_zerophase_filter(data_env, cutoff, fs, order)  # LP filtered version
+envf = boxcar_filter(data_env, BoxSize)  # LP filtered version
 
 sublen = npts   # number of points in sub-segment to view
+plotstart = 30        # minutes from beginning where plot starts
+plotminutes = 360      # minutes duration to show on plot
 
-t = np.arange(0, (sublen-0.5)/samprate, 1/samprate)
-# plt.plot(t, np.square(data_env)/30, 'y-', linewidth=0.5, label='envelope')
-plt.plot(t, np.square(envf), 'k-', linewidth=2, label='LP filtered')
-plt.title(st[0].stats.starttime)
-plt.ylabel('Data Envelope')
-plt.xlabel('Time [s]')
-plt.grid()
-plt.legend()
-# plt.subplots_adjust(hspace=0.3)
-plt.show()
+sph = samprate*60*60  # samples per hour, from samples per second
+spm = samprate*60     # samples per minute, from samples per second
+t = np.arange(0, (sublen-0.5)/spm, 1/spm)
+
+StartPlt = int(0)
+plotSpan = int(plotminutes * spm)
+
+while (StartPlt <= npts):
+  EndPlt = StartPlt + plotSpan     # min * samples/min = samples
+  if (EndPlt >= npts):
+    EndPlt = npts
+  plt.figure(num=1, figsize=(16,4))          # window size in inches
+  plt.plot(t[StartPlt:EndPlt], np.log(np.square(envf[StartPlt:EndPlt])), 'k-', linewidth=0.5, label=filename)
+  plt.title(st[0].stats.starttime)
+  plt.ylabel('Data Envelope')
+  plt.xlabel('Time [m]')
+  plt.grid()
+  plt.legend()
+  # plt.subplots_adjust(hspace=0.3)
+  plt.show()
+  StartPlt += plotSpan  # move on to next section of data
