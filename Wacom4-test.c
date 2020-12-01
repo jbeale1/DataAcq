ca494a966dff6792d11e440744cb7a9bbe9232cd
John Beale <jbeale1@users.noreply.github.com>
2020-12-01 14:09:47 -0800

    Update Wacom4-test.c

--- Wacom4-test.c
+++ Wacom4-test.c
@@ -1,9 +1,10 @@
+// Test of Wacom tablet (WACOM IV serial protocol)
+// J.Beale 01-DEC-2020   github.com/jbeale1
 
-// ultra-crude test of Wacom ArtZ II serial tablet (WACOM IV serial protocol)
-// connected through USB-Serial adaptor on ttyUSB0
-// from  stackoverflow.com/questions/6947413/how-to-open-read-and-write-from-serial-port-in-c/38318768#38318768
-
-#define TERMINAL    "/dev/ttyUSB0"
+// Protocol: github.com/jigpu/linuxwacom-wiki-archive/blob/master/wiki/Serial_Protocol_IV.md
+// Serial I/O: stackoverflow.com/questions/6947413/
+// X11 events: www.kernel.org/doc/html/v4.14/input/event-codes.html
+// Userland mouse driver example: gitlab.com/DarkElvenAngel/serial-mouse
 
 #include <errno.h>
 #include <fcntl.h> 
@@ -12,6 +13,9 @@
 #include <string.h>
 #include <termios.h>
 #include <unistd.h>
+
+#define SERPORT  "/dev/ttyUSB0"  // tablet serial device name
+#define PACKETSIZE 7             // basic Wacom IV packet without tilt
 
 int set_interface_attribs(int fd, int speed)
 {
@@ -64,19 +68,39 @@ void set_mincount(int fd, int mcount)
         printf("Error tcsetattr: %s\n", strerror(errno));
 }
 
+// ---------------------------------------------
+// extract tablet data from pkt[] and take action
+
+void parsePacket(unsigned char pkt[])
+{
+     // show raw 7-byte Wacom-IV packet in hex
+     // for (unsigned int j=0;j<7;j++) printf("%02x ",pkt[j]);
+     
+     int px = ((pkt[0] & 0x40) == 0x40);  // pen in range?
+     int xp = pkt[1]*128 + pkt[2];        // X coordinate
+     int yp = pkt[4]*128 + pkt[5];        // Y coordinate
+     int zp = (pkt[6]&0x3f) + px*0x40*(pkt[6]<0x40);  // pen pressure          
+     int b1 = (pkt[3] & 0x10) == 0x10; // Button 1 
+     int b2 = (pkt[3] & 0x20) == 0x20; // Button 2 (& eraser)
+     
+      // (x,y), presure, button1, button2/eraser, proximity
+     printf("%05d , %05d , %03d , %d, %d, %d\n",xp,yp,zp,b1,b2,px);      
+}
+
 // ====================================================================
 int main()
 {
-    char *portname = TERMINAL;
+    char *portname = SERPORT;
     int fd;
     int wlen;
     
-    char *xstr = "\r#\r"; // reset protocol to WACOM IV
-    // char *xstr = "~C\r"; // report max (X,Y)
-    //char *xstr = "~#\r";  // report Model/ROM
+        // xstr is command sent to device at start
+    char *xstr = "\r#\r"; // reset protocol to WACOM IV (7 byte packet)
+    // char *xstr = "~C\r";    // report max (X,Y) coords
+    // char *xstr = "~#\r";    // report Model/ROM
     // char *xstr = "~R\r";    // report config
-    // char *xstr = "FM1\r";    // enable tilt protocol
-    // FM1	Enable extra protocol for tilt management
+    // char *xstr = "FM1\r";   // enable tilt protocol (9-byte packet)
+    
     int xlen = strlen(xstr);
 
     fd = open(portname, O_RDWR | O_NOCTTY | O_SYNC);
@@ -84,109 +108,82 @@ int main()
         printf("Error opening %s: %s\n", portname, strerror(errno));
         return -1;
     }
-    /*baudrate 115200, 8 bits, no parity, 1 stop bit */
-    // set_interface_attribs(fd, B115200);
-    set_interface_attribs(fd, B9600);
-    //set_mincount(fd, 0);                /* set to pure timed read */
 
-    /* simple output */
-    wlen = write(fd, xstr, xlen);
+    set_interface_attribs(fd, B9600); // 9600 baud 8-N-1
+    //set_mincount(fd, 0);            // set to pure timed read
+
+    wlen = write(fd, xstr, xlen);  // send a command to serial device
     if (wlen != xlen) {
         printf("Error from write: %d, %d\n", wlen, errno);
     }
-    tcdrain(fd);    /* delay for output */
+    tcdrain(fd);    // delay for output
 
     unsigned int i=0;  // index into packet buffer pkt[]
 
-    /* simple noncanonical input */
+
+    // simple noncanonical serial input, in endless loop
     do {
-        unsigned char buf[80];  // raw set of serial input data
-        unsigned char pkt[10];  // WACOM IV data packet (7 or 9 chars)
+        unsigned char buf[80];  // single-read serial input data buffer
+        unsigned char pkt[10];  // one WACOM IV packet (7 or 9 chars)
         int rdlen;
 
         rdlen = read(fd, buf, sizeof(buf) - 1);
         if (rdlen > 0) {
-
-// #define DISPLAY_STRING
-
-#ifdef DISPLAY_STRING
-            buf[rdlen] = 0;
-            printf("Read %d: \"%s\"\n", rdlen, buf);
-#else /* display hex */
-
             unsigned char *p;
-            // printf("Read %d:", rdlen);
-            for (p = buf; rdlen-- > 0; p++) {
-                if (*p & 0x80) i=0;
-                pkt[i++] = *p;
-                if (i > 7) {
-                    printf("Error i=%d\n",i);
+
+            for (p = buf; rdlen-- > 0; p++) { // work over rec'd bytes
+                
+                if (*p & 0x80) i=0; // b7 is start-of-packet flag
+                pkt[i++] = *p;      // add this byte to packet
+                
+                if (i > PACKETSIZE) {
+                    printf("Packet Error, length=%d\n",i);
                     i=0;
                 }
-            }  // watch out for buffer overrun
-            if (i == 7) {
-                
-              //for (unsigned int j=0;j<7;j++) {
-              //  printf("%02x ",pkt[j]);
-              // }
-              int xh = ((int32_t) pkt[1]); // high part of X coord
-              int xl = ((int32_t) pkt[2]); // low bits of X coord
-              int xp = xh*128 + xl;
-              int yp = ((int32_t)pkt[4])*128 + pkt[5];
-              int zp = (int32_t) (pkt[6] & 0x3f) + 0x40*((pkt[6] < 0x40));
-              printf("%d , %d , %d\n",xp,yp,zp);  // x,y coords
-              // printf("\n");
+            } 
+            
+            if (i == PACKETSIZE) {       // complete packet received
+              parsePacket(pkt);  // extract the data and take action
               i = 0;
             }
-#endif
+
         } else if (rdlen < 0) {
             printf("Error from read: %d: %s\n", rdlen, strerror(errno));
         } else {  /* rdlen == 0 */
             printf("Timeout from read\n");
         }               
-        /* repeat read to get full message */
-    } while (1);
-}
+
+    } while (1); // repeat read to get rest of data
+    
+} // end main
+
 
 /*
- *   7-byte protocol
+ *   7-byte Wacom IV protocol
  * 
 e0 23 0c 00 26 21 40 
 e0 23 0e 00 26 26 40 
 e0 23 11 00 26 2a 40 
-e0 23 13 00 26 37 40 
-e0 23 10 00 26 53 40 
-e0 23 08 00 26 61 40 
-e0 22 7f 00 26 71 40 
-*
 * 
-* 
-*    9-byte protocol
+*    9-byte Wacom IV protocol
 *
 e0 0a 62 00 20 6b 40 2f 06 
 e0 0a 64 00 20 74 40 2f 06 
 e0 0a 68 00 21 00 40 30 05 
 e0 0a 6b 00 21 06 40 31 05 
-e0 0a 71 00 21 12 40 31 04 
-e0 0a 77 00 21 1d 40 32 04 
-e0 0a 7a 00 21 22 40 32 03 
-e0 0b 02 00 21 2b 40 33 03 
+*
 * 
+*  X pos, Y pos, Pressure, B1, B2, Proximity
 * 
-* 
- X   ,  Y   , Z (pressure)
-2640 , 4987 , 61
-2622 , 4987 , 60
-2575 , 4983 , 58
-2560 , 4982 , 58
-2529 , 4976 , 57
-2501 , 4970 , 57
-2475 , 4962 , 56
-2451 , 4954 , 54
-2420 , 4940 , 51
-2401 , 4930 , 50
-2392 , 4924 , 50
-2384 , 4918 , 50
-
+03909 , 04887 , 000 , 0, 0, 1
+03907 , 04887 , 003 , 0, 0, 1
+03903 , 04887 , 010 , 0, 0, 1
+03902 , 04889 , 021 , 0, 0, 1
+03902 , 04889 , 057 , 0, 1, 1
+03911 , 04880 , 105 , 0, 1, 1
+03918 , 04872 , 103 , 0, 1, 1
+03931 , 04869 , 071 , 0, 1, 1
+03940 , 04850 , 008 , 0, 1, 1
+03946 , 04835 , 000 , 0, 0, 0  <== pen leaves active area
 
 */
