992be86bcabf55c9d067045193d8db0f68609abd
John Beale <7452231+jbeale1@users.noreply.github.com>
2022-10-30 22:18:53 -0700

    use more I2C buses for multiple sensors

--- BMP280-demo.py
+++ BMP280-demo.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
-# based on Adafruit example code for BMP280 I2C pressure/temp sensor
+# Read several sensors, by creating more I2C buses on GPIO
+# https://www.instructables.com/Raspberry-PI-Multiple-I2c-Devices/
+# Sensor code based on Adafruit BMP280 example
 # J.Beale 2022-Oct-30
 
 import datetime
@@ -14,33 +16,43 @@ except ImportError:
 
 fnameOut = "tp-log1.csv"
 
-# Initialise the BMP280
-bus = SMBus(1)
-bmp280 = BMP280(i2c_dev=bus)
+bmp280 = []       # will be list of I2C devices
+i2cDev = [1,3,4]  # I2C buses with devices
 
-nAvg = 187  # how many readings to average (about 1 group / 10 sec)
+# Initialise the BMP280 on each bus
+for bus in i2cDev:
+  bmp280.append( BMP280(i2c_dev=SMBus(bus)) )
+
+nAvg = 187  # how many readings to average
 
 lineCount = 0
 
 with open(fnameOut, 'a') as f:
+  headers = "date_time, epoch, T1,T2,T3, P1,P2,P3"
+  f.write("%s\n" % headers)
   while True:
-    degC = 0
-    pres = 0
-    for i in range(nAvg):
-      degC += bmp280.get_temperature()
-      pres += bmp280.get_pressure()
+    cnt = len(i2cDev) # how many devices there are
+    degC = [0] * cnt
+    pres = [0] * cnt
+    for n in range(nAvg):
+      for i in range(cnt):
+        degC[i] += bmp280[i].get_temperature()
+        pres[i] += bmp280[i].get_pressure()
       time.sleep(0.05)
 
-    degC /= nAvg
-    pres /= nAvg
+    for i in range(cnt): 
+      degC[i] /= nAvg
+      pres[i] /= nAvg
+
     now = datetime.datetime.now()
     sec = time.time()
 
-    outString = ('%s, %d, %05.3f, %05.3f' % (now, sec, degC, pres))
+
+    outString = ('%s, %d, %05.3f, %05.3f, %05.3f, %05.3f, %05.3f, %05.3f' % (now, sec, 
+      degC[0], degC[1], degC[2],
+      pres[0], pres[1], pres[2]))
     print(outString)
     f.write('%s\n' % outString)
-
     lineCount += 1
     if (lineCount % 10) == 0:
       f.flush()
-
